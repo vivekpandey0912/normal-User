1187b2ebd733ccc438b97bb61fe29214695b2819
Vivek Pandey <vp1446392@gmail.com>
2023-07-30 00:34:20 +0530

    method created for register user normal

--- src/main/java/com/vivek/bej/usermodule/service/UserService.java
+++ src/main/java/com/vivek/bej/usermodule/service/UserService.java
@@ -1,4 +1,10 @@
 package com.vivek.bej.usermodule.service;
 
+import com.vivek.bej.usermodule.domain.User;
+import com.vivek.bej.usermodule.exception.UserAlreadyExist;
+
 public interface UserService {
+
+    public User registerAdmin(User user) throws UserAlreadyExist;
+
 }
