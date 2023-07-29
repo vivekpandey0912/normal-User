5f3f9249f2f99651bd5870556e2851b3d3cd9bf9
Vivek Pandey <vp1446392@gmail.com>
2023-07-30 00:34:55 +0530

    implementing interface with method register new user normal user

---
+++ src/main/java/com/vivek/bej/usermodule/service/UserServiceImpl.java
@@ -0,0 +1,11 @@
+package com.vivek.bej.usermodule.service;
+
+import com.vivek.bej.usermodule.domain.User;
+import com.vivek.bej.usermodule.exception.UserAlreadyExist;
+
+public class UserServiceImpl implements UserService {
+    @Override
+    public User registerAdmin(User user) throws UserAlreadyExist {
+        return null;
+    }
+}
